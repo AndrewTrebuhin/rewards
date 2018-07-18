9deabb1fdf1772ff56d57127e920e7ce369e818c
Andrii Trebukhin <andrewtrebuhin@gmail.com>
2018-07-18 20:54:00 +0300

    Add name to user

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,12 +1,12 @@
 class Reward < ApplicationRecord
   belongs_to :user
 
+  scope :filter_by_status, ->(status) { where(status: status) if status }
+
   enum status: { waiting_to_be_approved: 0,
                  approved: 1,
                  denied: 2,
                  sent: 3,
                  redeemed: 4,
                  completed: 5 }
-
-  scope :filter_by_status, ->(status) { where(status: status) if status }
 end
