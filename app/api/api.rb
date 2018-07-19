07a323aa1e51d3aaaebe0bb283e8e2d397823b7b
Andrii Trebukhin <andrewtrebuhin@gmail.com>
2018-07-19 19:49:19 +0300

    Add swagger

--- app/api/api.rb
+++ app/api/api.rb
@@ -2,4 +2,8 @@ class API < Grape::API
   format :json
 
   mount V1::Base
+
+  add_swagger_documentation info: { title: 'My Rewards API Documentation' },
+                            api_version: :v1,
+                            hide_documentation_path: true
 end
